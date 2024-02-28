599b50d318a7876e79e3655d3baf2817ad8ff05e
Marius Vizbras <marius.viz@gmail.com>
2024-02-28 15:13:47 +0200

    :white_check_mark: add playwright test

--- e2e/vue.spec.ts
+++ e2e/vue.spec.ts
@@ -1,8 +1,20 @@
 import { test, expect } from '@playwright/test';
 
-// See here how to get started:
-// https://playwright.dev/docs/intro
 test('visits the app root url', async ({ page }) => {
   await page.goto('/');
-  await expect(page.locator('div.greetings > h1')).toHaveText('You did it!');
-})
+  await expect(page.locator('div.greetings > h1')).toHaveText('Make Shopping great again!');
+  await page.getByRole('link', { name: 'Healthy food', exact: true }).click();
+  await page.getByRole('row', { name: 'Whole Grains Wheat add' }).getByRole('img').click();
+  await page.getByRole('link', { name: 'Shopping List' }).click();
+  await page.getByPlaceholder('Add product').click();
+  await page.getByPlaceholder('Add product').fill('apple');
+  await page.getByPlaceholder('Add product').press('Enter');
+  await page.getByPlaceholder('Add product').click();
+  await page.getByPlaceholder('Add product').fill('banana');
+  await page.getByPlaceholder('Add product').press('Enter');
+  await page.locator('li').filter({ hasText: 'banana' }).getByRole('img').click();
+  await page.getByRole('link', { name: 'healthy food', exact: true }).click();
+  await page.getByPlaceholder('Search product...').fill('or');
+  await page.getByRole('cell', { name: 'Oranges' }).click();
+  await page.getByPlaceholder('Search product...').click();
+});
